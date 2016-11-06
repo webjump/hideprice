35b75e7db8c0208f446709948eac9cac13ef6a71
Yogendra Kumar <yogendrakumar125@gmail.com>
2016-11-06 10:00:24 +0530

    Initial Commit

---
+++ Helper/Data.php
@@ -0,0 +1,49 @@
+<?php
+namespace Estdevs\Hideprice\Helper;
+
+class Data extends \Magento\Framework\App\Helper\AbstractHelper
+{
+    const XML_CONFIG_ENABLE = 'hideprice/configuration/enablehideprice';
+    const XML_PRICE_TEXT = 'hideprice/configuration/price_text';
+       const XML_EST_MESSAGE = 'hideprice/configuration/price_message';
+
+    protected $_scopeConfig;
+    protected $enable_config;
+    protected $price_config;
+    protected $customerSession;
+    protected $estmessage;
+
+    public function __construct(
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Customer\Model\Session $customerSession
+    ) {
+        $this->_scopeConfig = $scopeConfig;
+        $this->customerSession = $customerSession;
+        $this->enable_config =  $this->_scopeConfig->getValue(self::XML_CONFIG_ENABLE);
+        $this->price_config =  $this->_scopeConfig->getValue(self::XML_PRICE_TEXT);
+        $this->estmessage =  $this->_scopeConfig->getValue(self::XML_EST_MESSAGE);
+    }
+
+    public function isenable()
+    {
+        return  $this->enable_config;
+    }
+
+    public function getbuttontext()
+    {
+      return  empty($this->price_config)?'login to see price':$this->price_config;
+        // return  $this->price_config;
+    }
+
+    public function estmessage()
+    {
+      return  empty($this->estmessage)?'call at 842790****':$this->estmessage;
+        // return  $this->price_config;
+    }
+
+    public function isCustomerlogin()
+    {
+
+        return $this->customerSession->isLoggedIn()?true:false;
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Hide Price 
+
+Thank you for using the "Estoredevs Hideprice module for Magento2" (Estdevs_Hideprice).
+
+## Contents
+
+- [Description](#description)
+
+## Description
+
+Estdevs_Hideprice
+
+Hide Price is the most powerful extension for hiding the product price from non-logedIn users. This extension encourages users to register on the website.you can hide add to cart button if you want. To See the product price user required to register and login. Admin can configure the section from admin panel. admin can hide the price as well as the add to cart button. admin simply choose the hide price yes to hide the price. when price hide it will say "please login to view price". 
